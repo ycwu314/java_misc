3239405e07fa50cc7d6565fcaad0ee85482eb743
ycwu314 <535215779@qq.com>
2017-01-25 14:01:03 +0800

    move package

---
+++ src/main/java/org/ycwu/annotation/example/Before.java
@@ -0,0 +1,16 @@
+package org.ycwu.annotation.example;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Before {
+
+	boolean log() default false;
+}
---
+++ src/main/java/org/ycwu/annotation/example/Range.java
@@ -0,0 +1,19 @@
+package org.ycwu.annotation.example;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ METHOD, PARAMETER })
+public @interface Range {
+
+	int min() default 1;
+
+	int max() default 100;
+}
